2b68deb98014abf690d628b53cdb65a212c8f896
Nish-Hub <nishantgsingh07@gmail.com>
2018-01-22 04:55:25 +0530

    Removed sysouts

--- StringCalculator/src/com/calc/processor/ChangeName.java
+++ StringCalculator/src/com/calc/processor/ChangeName.java
@@ -6,13 +6,9 @@ public class ChangeName {
 
 public void	process(StringBuffer name,Entity e)
 {
-	System.out.println("changed name is "+name.toString().substring(0,1).toUpperCase().concat(name.substring(1,name.length())));
-	//name=new StringBuffer(name.toString().substring(0,1).toUpperCase().concat(name.substring(1,name.length())));
 	String s=name.toString().substring(0,1).toUpperCase().concat(name.substring(1,name.length()));
-	System.out.println("before :  name is "+name);
-	//name=new StringBuffer(s);
 	name.delete(0, name.length());
 	name.append(s);
-	System.out.println("after :  name is "+name);
+	
 }
 }
